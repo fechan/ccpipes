a0109ce9350436aa4c4340ce520bb6b61ce7c215
fechan <fred@fredchan.org>
2024-04-10 01:52:14 -0700

    initialize chests differently

--- computercraft/machine.lua
+++ computercraft/machine.lua
@@ -1,17 +1,27 @@
 Machine = {}
 
----Initialize a machine from a CC peripheral ID using default values.
----
----Each inventory slot will be assigned to a different group.
+---Initialize a new machine
 ---@param periphId string CC Peripheral ID
----@return table machine New machine
-function Machine.fromPeriphId (periphId)
-  local o = {
-    nickname = nil,
+---@param groups table? List of starting slot groups. If `nil` is passed, the groups attribute will be an empty table.
+---@param nickname string? Machine name
+---@return Machine machine New machine
+function Machine.new (periphId, groups, nickname)
+  return {
     periphId = periphId,
+    groups = groups or {},
+    nickname = nickname,
   }
+end
 
-  o.groups = {}
+---Initialize a machine from a CC peripheral where every slot has its own group
+---@param periphId string CC Peripheral ID
+---@return Machine machine New machine
+function Machine.fromPeriphId (periphId)
+  local o = Machine.new(periphId)
+
+  if string.find(periphId, "minecraft:chest_") then
+    return Machine.fromChestPeriphId(periphId)
+  end
 
   local peripheral = peripheral.wrap(periphId)
 
@@ -20,16 +30,46 @@ function Machine.fromPeriphId (periphId)
     local slot = {
       periphId = periphId,
       slot = slotNbr,
-      outputs = {},
-      distribution = 'roundRobin',
     }
 
     local group = {
-      slots = {slot}
+      slots = {slot},
+      distribution = 'roundRobin',
+      outputs = {},
+      nickname = nil,
     }
 
     table.insert(o.groups, group)
   end
+
+  return o
+end
+
+---Initialize a chest machine from a CC peripheral where every slot is in one big group
+---@param periphId string CC Peripheral ID
+---@return Machine machine New machine
+function Machine.fromChestPeriphId (periphId)
+  local o = Machine.new(periphId)
+
+  local group = {
+    slots = {},
+    distribution = 'roundRobin',
+    outputs = {},
+    nickname = 'inventory',
+  }
+
+  local peripheral = peripheral.wrap(periphId)
+
+  for slotNbr=1, peripheral.size() do
+    local slot = {
+      periphId = periphId,
+      slot = slotNbr,
+    }
+
+    table.insert(group.slots, slot)
+  end
+
+  table.insert(o.groups, group)
 
   return o
 end
