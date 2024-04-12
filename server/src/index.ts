f9e0131cf7e0f0977d26c91d50976867a1fe3be4
fechan <fred@fredchan.org>
2024-04-12 00:06:37 -0700

    relay all messages other session related ones

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,16 @@ import { WebSocket, WebSocketServer } from "ws";
 import { FailResponse, Request, SessionCreateReq, SessionJoinReq, SuccessResponse } from "./types/messages";
 import { Session, SessionId } from "./types/session";
 
+type Role = ('CC' | 'editor');
+
 const wss = new WebSocketServer({ port: 3000 });
 
 const sessions: { [key: SessionId]: Session } = {};
 
 wss.on("connection", function connection(ws) {
+  let sessionId: string;
+  let role: Role;
+
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
@@ -15,32 +20,62 @@ wss.on("connection", function connection(ws) {
 
     if ("reqId" in message) {
       const request: Request = message;
+      
       switch (request.type) {
         case "SessionCreate":
-          createSession(request as SessionCreateReq, ws);
+          sessionId = createSession(request as SessionCreateReq, ws);
+          if (sessionId) role = 'CC';
           break;
-
+          
         case "SessionJoin":
-          joinSession(request as SessionJoinReq, ws);
+          sessionId = joinSession(request as SessionJoinReq, ws);
+          if (sessionId) role = 'editor';
+          break;
       
         default:
+          const destination = role === 'CC' ? 'editor' : 'CC';
+          relayMessage(json, sessionId, destination);
           break;
       }
-
     }
   });
 });
 
+/**
+ * Relay a message from the editor to ComputerCraft or vice versa
+ * @param message Message to relay
+ * @param sessionId Session ID
+ * @param to Websocket to relay to
+ */
+function relayMessage(message: string, sessionId: SessionId, to: Role) {
+  if (to === 'CC') {
+    sessions[sessionId].editor.send(message);
+  } else {
+    sessions[sessionId].computerCraft.send(message);
+  }
+}
+
+/**
+ * Send a generic success response for the given request ID
+ * @param reqId Reqeust ID
+ * @param ws Websocket to send to
+ */
 function sendGenericSuccess(reqId: string, ws: WebSocket) {
   const res: SuccessResponse = {
     type: "ConfirmationResponse",
     ok: true,
     reqId: reqId,
   };
-  return ws.send(JSON.stringify(res));
+  ws.send(JSON.stringify(res));
 }
 
-function joinSession({ reqId, sessionId }: SessionJoinReq, editor: WebSocket) {  
+/**
+ * Join an editor to a session
+ * @param param0 Session join request 
+ * @param editor Websocket of the editor
+ * @returns Session ID on success, undefined on failure
+ */
+function joinSession({ reqId, sessionId }: SessionJoinReq, editor: WebSocket): SessionId {  
   if (!(sessionId in sessions)) {
     const res: FailResponse = {
       type: "ConfirmationResponse",
@@ -66,9 +101,16 @@ function joinSession({ reqId, sessionId }: SessionJoinReq, editor: WebSocket) {
   sessions[sessionId].editor = editor;
 
   sendGenericSuccess(reqId, editor);
+  return sessionId;
 }
 
-function createSession({ reqId, sessionId }: SessionCreateReq, computerCraft: WebSocket) {
+/**
+ * Create a session and add the ComputerCraft computer to it
+ * @param param0 Session create request
+ * @param computerCraft Websocket of the CC computer
+ * @returns Session ID on success, undefined on failure
+ */
+function createSession({ reqId, sessionId }: SessionCreateReq, computerCraft: WebSocket): SessionId {
   if (sessionId in sessions) {
     const res: FailResponse = {
       type: "ConfirmationResponse",
@@ -86,4 +128,5 @@ function createSession({ reqId, sessionId }: SessionCreateReq, computerCraft: We
   };
 
   sendGenericSuccess(reqId, computerCraft);
+  return sessionId;
 }
