9f78c93cc6880e5c4576704ef7aab679d68fce41
fechan <fred@fredchan.org>
2024-04-17 01:32:58 -0700

    make bg of combine target green

--- client/src/nodes/MachineNode.tsx
+++ client/src/nodes/MachineNode.tsx
@@ -1,14 +1,20 @@
 import { Machine } from "@server/types/core-types";
-import type { NodeProps } from "reactflow";
+import type { NodeProps, NodeTypes } from "reactflow";
 
 export type MachineNodeData = {
   machine: Machine,
+  intersectedBy? : keyof NodeTypes,
 };
 
 export function MachineNode({ data }: NodeProps<MachineNodeData>) {
-  const { machine } = data;
+  const { machine, intersectedBy } = data;
   return (
-    <div className="react-flow__node-default w-full h-full">
+    <div 
+      className={ 
+        "react-flow__node-default w-full h-full" +
+        (intersectedBy === "machine" ? " bg-green-200" : "")
+      }
+    >
       <div className="absolute -top-5 left-0">
         { machine.nickname || machine.id }
         { machine.nickname && 
