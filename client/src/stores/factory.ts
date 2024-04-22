aa52cb607fe067bd9d76d52380ea9b32f59fa355
fechan <fred@fredchan.org>
2024-04-21 22:07:28 -0700

    detect adds/deletes after patches

--- client/src/stores/factory.ts
+++ client/src/stores/factory.ts
@@ -2,9 +2,24 @@ import { Factory, GroupId, MachineId } from "@server/types/core-types";
 import { Delta, patch } from "jsondiffpatch";
 import { create } from "zustand";
 
+interface AddsAndDeletes {
+  adds: string[],
+  deletes: string[],
+}
+
+interface FactoryAddsAndDeletes {
+  pipes: AddsAndDeletes,
+  machines: AddsAndDeletes,
+  groups: AddsAndDeletes,
+}
+
 interface FactoryStore {
+  /** Factory object */
   factory: Factory,
+  /** Map from group IDs in the factory to their parent machine's ID */
   groupParents: {[key: GroupId]: MachineId},
+  /** IDs of added or deleted nodes from the last factory update/patch */
+  addsAndDeletes: FactoryAddsAndDeletes,
   setFactory: (factory: Factory) => void,
   patchFactory: (diffs: Delta[]) => void,
 };
@@ -13,6 +28,12 @@ const emptyFactory: Factory = {
   machines: {},
   pipes: {},
   groups: {},
+};
+
+const emptyFactoryAddsAndDeletes: FactoryAddsAndDeletes = {
+  machines: {adds: [], deletes: []},
+  pipes: {adds: [], deletes: []},
+  groups: {adds: [], deletes: []},
 };
 
 /**
@@ -30,24 +51,54 @@ function getGroupParents(factory: Factory) {
   return groupParents;
 }
 
+function collectAddsAndDeletes(diff: Delta, addsAndDeletes?: FactoryAddsAndDeletes) {
+  if (diff === undefined) {
+    return;
+  }
+
+  if (addsAndDeletes === undefined) {
+    addsAndDeletes = {
+      machines: {adds: [], deletes: []},
+      pipes: {adds: [], deletes: []},
+      groups: {adds: [], deletes: []},
+    };
+  }
+
+  for (let componentType of Object.keys(diff)) {
+    if (["machines", "pipes", "groups"].includes(componentType)) {
+      for (let [componentId, delta] of Object.entries(diff[componentType])) {
+        if (delta.length === 1) {
+          addsAndDeletes[componentType].adds.push(componentId);
+        } else if (delta.length === 3) {
+          addsAndDeletes[componentType].deletes.push(componentId);
+        }
+      }
+    }
+  }
+
+  return addsAndDeletes;
+}
+
 export const useFactoryStore = create<FactoryStore>()(set => ({
   factory: emptyFactory,
   groupParents: {},
+  addsAndDeletes: emptyFactoryAddsAndDeletes,
   setFactory: factory => set(() => ({
     factory: factory,
     groupParents: getGroupParents(factory),
   })),
   patchFactory: diffs => set(state => {
     let updatedFactory = state.factory;
+    let addsAndDeletes: FactoryAddsAndDeletes | undefined;
     for (let diff of diffs) {
       updatedFactory = patch(updatedFactory, diff) as Factory;
+      addsAndDeletes = collectAddsAndDeletes(diff, addsAndDeletes)
     }
-
-    console.log(updatedFactory)
 
     return {
       factory: updatedFactory,
       groupParents: getGroupParents(updatedFactory),
+      addsAndDeletes: addsAndDeletes,
     };
   })
 }));
