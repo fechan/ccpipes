3edb0afedbe86bddf571182b9e3c059868635ea7
fechan <fred@fredchan.org>
2024-04-27 23:52:23 -0700

    add more variation for similar strings

--- client/src/StringToColor.ts
+++ client/src/StringToColor.ts
@@ -1,18 +1,9 @@
-/**
- * Turn a string into a 6-digit hex color.
- * Adapted from https://stackoverflow.com/a/16348977
- * @param str String to turn into a color
- * @returns 6-digit hex color
- */
-export const stringToColor = (str: string) => {
+export const stringToColor = (string: string, saturation = 100, lightness = 20) => {
   let hash = 0;
-  str.split('').forEach(char => {
-    hash = char.charCodeAt(0) + ((hash << 5) - hash)
-  })
-  let color = '#'
-  for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xff
-    color += value.toString(16).padStart(2, '0')
+  for (let i = 0; i < string.length; i++) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    hash = hash & hash;
   }
-  return color
+  hash = 100 * hash
+  return `hsl(${(hash % 360)}, ${saturation}%, ${lightness}%)`;
 }
