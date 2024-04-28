8d1aed1bb4620f7c75525fe6e21cadbdb279cd98
fechan <fred@fredchan.org>
2024-04-27 22:34:02 -0700

    make the new session modal minecrafty

--- client/src/components/NewSessionModal.tsx
+++ client/src/components/NewSessionModal.tsx
@@ -20,9 +20,15 @@ export function NewSessionModal({ sendMessage }: NewSessionModalData) {
   }
 
   return (
-    <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full md:inset-0 bg-black/50">
-      <div className="relative p-4 max-w-2xl max-h-full bg-white border rounded-lg shadow">
+    <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full md:inset-0 bg-black/70">
+      <div
+        className={`
+          relative p-4 max-w-1xl max-h-full rounded shadow
+          bg-mcgui-bg border-2 border-t-mcgui-border-light border-s-mcgui-border-light border-b-mcgui-border-dark border-e-mcgui-border-dark
+        `}
+      >
         <iframe name="dummy" id="dummy" className="hidden"></iframe>
+        <header className="mb-5">Welcome to the CCPipes editor!</header>
         <form target="dummy" className="flex flex-col items-center">
           <label className="mb-3" htmlFor="sessionCode">Enter the code displayed on the ComputerCraft computer:</label>
           <div>
@@ -30,12 +36,23 @@ export function NewSessionModal({ sendMessage }: NewSessionModalData) {
               name="sessionCode"
               id="sessionCode"
               type="text"
-              className="bg-gray-50 border border-gray-300 rounded-lg p-2.5 me-3 h-10 shadow-inner"
+              className={`
+                p-2.5 me-3 h-10 shadow-inner text-white
+                bg-black border-2 border-b-mcgui-slot-border-light border-e-mcgui-slot-border-light border-t-mcgui-slot-border-dark border-s-mcgui-slot-border-dark
+              `}
+              style={{textShadow: "2px 2px 0px #373737"}}
               onInput={ e => setSessionId((e.target as HTMLInputElement).value.toUpperCase()) }
               value={ sessionId }
             />
             <button
-              className="rounded-lg disabled:bg-gray-500 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white w-40 h-10"
+              className={`
+                disabled:border-black disabled:border-2 disabled:text-stone-500 disabled:bg-stone-800
+                hover:border-white hover:border-4
+                active:bg-blue-700
+                text-white w-40 h-10
+                bg-neutral-500 border-2 border-t-mcgui-slot-border-light border-s-mcgui-slot-border-light border-b-mcgui-slot-border-dark border-e-mcgui-slot-border-dark
+              `}
+              style={{textShadow: "1.5px 1.5px 0px #373737"}}
               onClick={ joinSession }
               disabled={ sessionId.length < 1 }
             >
