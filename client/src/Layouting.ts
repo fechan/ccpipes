61ba11f8a1fa387634a8a02bc5b3e1443a47cea3
fechan <fred@fredchan.org>
2024-05-22 01:05:14 -0700

    accomplish the same without the second round

--- client/src/Layouting.ts
+++ client/src/Layouting.ts
@@ -54,74 +54,33 @@ export async function getLayoutedElements(nodes: Node[], edges: Edge[], factory:
         height: getHeight(group.slots.length),
       };
       machineNodeElk.children!.push(groupNodeElk);
-      
-      if (group.x) {
-        groupNodeElk.x = group.x;
-        groupNodeElk.y = group.y;
-      }
     }
   }
 
   // fulfill width information first
   let layout = await elk.layout(graph);
 
-  // layout again with fixed x,y positions if they exist in the factory
-  const graph2: ElkNode = {
-    id: "root",
-    layoutOptions: elkOptions,
-    edges: edges.map(edge => ({ ...edge, sources: [edge.source], targets: [edge.target]})),
-    children: [],
-  }
-
-  for (let machineNodeElk of layout.children || []) {
-    const newMachineNodeElk: ElkNode = {
-      ...nodeMap[machineNodeElk.id],
-      layoutOptions: {
-        "elk.padding": "[top=30,right=30,bottom=30,left=30]",
-      },
-      x: machineNodeElk.x,
-      y: machineNodeElk.y,
-      width: machineNodeElk.width,
-      height: machineNodeElk.height,
-      children: [],
-    };
-
-    // if (x,y) position is defined in the factory, set the position and set noLayout
-    const machine = factory.machines[machineNodeElk.id];
-    if (machine.x) {
-      newMachineNodeElk.x = machine.x;
-      newMachineNodeElk.y = machine.y;
-      newMachineNodeElk.layoutOptions = { "elk.noLayout": "true" };
-    }
-
-    for (let groupNodeElk of machineNodeElk.children || []) {
-      newMachineNodeElk.children!.push({
-        ...nodeMap[groupNodeElk.id],
-        x: groupNodeElk.x,
-        y: groupNodeElk.y,
-        width: groupNodeElk.width,
-        height: groupNodeElk.height,
-      });
-    }
-
-    graph2.children?.push(newMachineNodeElk);
-  }
-
-  layout = await elk.layout(graph2);
-
   const layoutedNodes: Node[] = [];
   for (let machineNodeElk of layout.children!) {
+    const machine = factory.machines[machineNodeElk.id];
     layoutedNodes.push({
       ...machineNodeElk,
-      position: { x: machineNodeElk.x || 0, y: machineNodeElk.y || 0 },
+      position: {
+        x: machine.x || machineNodeElk.x || 0,
+        y: machine.y || machineNodeElk.y || 0
+      },
       style: { width: machineNodeElk.width, height: machineNodeElk.height },
       data: {},
     });
 
     for (let groupNodeElk of machineNodeElk.children!) {
+      const group = factory.groups[groupNodeElk.id];
       layoutedNodes.push({
         ...groupNodeElk,
-        position: { x: groupNodeElk.x || 0, y: groupNodeElk.y || 0 },
+        position: {
+          x: group.x || groupNodeElk.x || 0,
+          y: group.x || groupNodeElk.y || 0
+        },
         style: { width: groupNodeElk.width, height: groupNodeElk.height },
         parentId: machineNodeElk.id,
         data: {},
