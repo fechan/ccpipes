bf26f78491a9d34256f344706c5f9d7dc2c9c287
fechan <fred@fredchan.org>
2024-06-10 20:09:59 -0700

    make colors lighter

--- client/src/StringToColor.ts
+++ client/src/StringToColor.ts
@@ -1,4 +1,4 @@
-export const stringToColor = (string: string, saturation = 100, lightness = 20) => {
+export const stringToColor = (string: string, saturation = 100, lightness = 25) => {
   let hash = 0;
   for (let i = 0; i < string.length; i++) {
     hash = string.charCodeAt(i) + ((hash << 5) - hash);
